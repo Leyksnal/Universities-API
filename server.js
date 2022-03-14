24e800b8189e4a8e8690c11faf31ab681accb1dc
leyksnal <leyksnal@gmail.com>
2022-03-14 13:07:05 +0100

    heroku2

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const allRoutes = require('./route/route')
 const app = express()
 app.get('/', (req, res)=>{
     res.status(200).json({
-        message: "THIS IS THE SCHOOL API WELCOME"
+        message: "THIS IS THE SCHOOL API WELCOME, THANKS FOR YOUUR SURPPORT ASAKEMI"
     })
 })
 
